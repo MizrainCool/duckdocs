783fcd9bd6b4942708e370287c856bf13646cbcc
MizrainCool <misiaguilas@hotmail.com>
2024-03-26 19:29:37 -0600

    Primer commit

--- app/page.js
+++ app/page.js
@@ -1,94 +1,109 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    <main className={styles.subContainer}>
+      <div className={styles.header}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <Link href="#"><img src="/senate.svg" /></Link>
+          <Link href="#" style={{ textDecoration: "none" }}><p>SOLUCIONES</p></Link>
+          <Link href="#" style={{ textDecoration: "none" }}><p>RECURSOS</p></Link>
+        </div>
+        <button>INICIAR SESIÓN</button>
+      </div>
+      <div className={styles.banner}>
+        <div>
+          <h2>Una solución <b>legal</b> para cada etapa de tu negocio</h2>
+          <p>
+            Elige los servicios de nuestro catálogo que se ajustan 
+            a los objetivos de tu empres, con nosotros, todo es  rápido
+            y a un precio justo.
+          </p>
+        </div>
+        <div>
+          <img src="/laptop.svg" />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.bloqueBlanco}>
+        <h2>Servicios legales para iniciar tu negocio</h2>
+        <div className={styles.subBloque}>
+          <div style={{ textAlign: "center", width: "45%", display: "flex", alignItems: "center" }}>
+            <img src="/servicios.svg" />
+          </div>
+          <div style={{ width: "60%" }}>
+            <h3>Si vas a iniciar un nuevo negocio, emprendimiento o proyecto, estos servicios legales son para ti:</h3>
+            <div style={{ padding: "1vh",  boxShadow: ".5vh .5vh .5vh .1vh", borderRadius: "2vh", marginBottom: "2vh" }}>
+              <b>Constituye tu empresa</b>
+              <p>Formalizamos tu empresa ante corredor público, la inscribirmos en el RPPC y te entregamos tu Acta Constitutiva.</p>
+            </div>
+            <div style={{ padding: "1vh", boxShadow: ".5vh .5vh .5vh .1vh", borderRadius: "2vh", marginBottom: "2vh" }}>
+              <b>Registrar mi marca</b>
+              <p>Realizamos un dictamen para determinar la probabilidad de éxito en el registro y nos encargamos de la presentación ante el IMPI.</p>
+            </div>
+            <div style={{ padding: "1vh", boxShadow: ".5vh .5vh .5vh .1vh", borderRadius: "2vh", marginBottom: "2vh" }}>
+              <b>Crear un contrato</b>
+              <p>Una biblioteca de contratos disponibles, redactados por abogados y enfocados en cada necesidad de tu negocio.</p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.bloqueAzul}>
+        <h3>Carga tus propios documentos o utiliza nuestras plantillas inteligentes para crear y firmar tus contratos.</h3>
+        <div className={styles.cartas}>
+          <div className={styles.carta}>
+            <img src="/pm.svg" />
+            <p>Publicidad y marketing</p>
+          </div>
+          <div className={styles.carta}>
+            <img src="/fl.svg" />
+            <p>Finanzas y legal</p>
+          </div>
+          <div className={styles.carta}>
+            <img src="i.svg" />
+            <p>Inmobiliarias</p>
+          </div>
+          <div className={styles.carta}>
+            <img src="ed.svg" />
+            <p>Eventos y deportes</p>
+          </div>
+          <div className={styles.carta}>
+            <img src="r.svg" />
+            <p>Restaurantes</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div>
+          <h4>DuckDocs</h4>
+          <p>+1000 Clientes</p>
+          <p>+500 Empresas</p>
+          <p>+1000 Marcas</p>
+        </div>
+        <div>
+          <p>SERVICIOS</p>
+          <hr/>
+          <p>Constitución de empresas</p>
+          <p>Registro de marca</p>
+          <p>Biblioteca de contactos</p>
+          <p>API Firma digital</p>
+        </div>
+        <div>
+          <p>RECURSOS</p>
+          <hr/>
+          <p>Libro de ayuda legal</p>
+          <p>Blog</p>
+          <p>Facturar</p>
+          <p>Aviso de privacidad</p>
+        </div>
+        <div>
+          <p>REDES SOCIALES</p>
+          <hr/>
+          <p>Facebook</p>
+          <p>Instagram</p>
+          <p>Twitter</p>
+        </div>
       </div>
     </main>
   );
